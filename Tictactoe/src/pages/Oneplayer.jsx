16c32cb5e64adde2f0ca91653d362d511775599e
Taron Osifo <taron.osifo1@gmail.com>
2026-04-12 03:36:27 -0500

    Further changes for backend communication

--- Tictactoe/src/pages/Oneplayer.jsx
+++ Tictactoe/src/pages/Oneplayer.jsx
@@ -34,7 +34,7 @@ function Oneplayer() {
   // 🔥 update backend
   const saveScore = async (player, score) => {
     try {
-      await fetch("http://tictactoe-api.onrender.com/scores", {
+      await fetch("https://tictactoe-4-1hm1.onrender.com/scores", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ player, score })
