8efe25e0f106f49eeda99025d6e48345b2eee822
Taron Osifo <taron.osifo1@gmail.com>
2026-06-23 23:35:03 -0500

    Final changes for frontend connection to EC2 backend

--- Tictactoe/frontend/src/utils/api.js
+++ Tictactoe/frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 //utils/api.js
-const BASE_URL = "http://0.0.0.0:8000";
+const BASE_URL = "http://3.22.170.170:8000";
 
 export async function registerUser(username, password) {
     const res = await fetch(`${BASE_URL}/users/register`, {
